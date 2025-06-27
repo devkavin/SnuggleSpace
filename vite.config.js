2cf4736542b5d64d0aebe6bb0629b9b0bb586ae8
devkavin <kavindra.senanayake@gmail.com>
2025-06-28 02:53:15 +0530

    mixed content error fix

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,22 @@ export default defineConfig({
         }),
         react(),
     ],
+    server: {
+        https: false,
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+            },
+        },
+    },
 });
